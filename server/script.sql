0f1ea797c8a784b4b2829a76e44cdc0c92eb0e73
王子陶 <2451355023@qq.com>
2020-05-02 11:10:44 +0800

    添加健康信息表sql脚本

--- server/script.sql
+++ server/script.sql
@@ -1,3 +1,27 @@
+create table health_questionnaire
+(
+    id                  int auto_increment
+        primary key,
+    time                datetime default CURRENT_TIMESTAMP not null,
+    userid              int                                not null,
+    height              decimal(5, 2)                      null,
+    weight              decimal(5, 2)                      null,
+    waist_circumference decimal(5, 2)                      null,
+    body_temperature    decimal(5, 2)                      null,
+    heart_rate          decimal(5, 2)                      null,
+    lung_capacity       int                                null,
+    blood_pressure      decimal(5, 2)                      null,
+    blood_sugar         decimal(5, 2)                      null,
+    creat_by            int                                not null
+)
+    comment '健康调查表';
+
+create index health_questionnaire_time_index
+    on health_questionnaire (time);
+
+create index health_questionnaire_userid_time_index
+    on health_questionnaire (userid, time);
+
 create table image
 (
     id    int auto_increment,
@@ -8,6 +32,20 @@ create table image
 
 alter table image
     add primary key (id);
+
+create table login_history
+(
+    user int                                not null,
+    time datetime default CURRENT_TIMESTAMP not null,
+    constraint login_history_pk
+        unique (user, time)
+);
+
+create index login_history_time_index
+    on login_history (time);
+
+create index login_history_user_index
+    on login_history (user);
 
 create table user
 (
